5c0ea252378282e8baaeb28d7569a13456327e53
9jazeez <9jazeez@gmail.com>
2021-09-28 11:42:20 +0000

    Error3 message corrected

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,11 +5,11 @@ def safe_print_list(my_list=[], x=0):
     try:
 
         a = 0
-        if x == 0:
-            return (a)
-        for i in range(0, x):
-            print(my_list[i], end='')
-            a +=1
+        for i in my_list:
+            if x <= a:
+                break
+            print(i, end='')
+            a += 1
         print()
         return (a)
 
