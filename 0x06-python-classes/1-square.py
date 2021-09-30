891b0b08e8c2f809c8e92d6eff81b95193faa8b2
9jazeez <9jazeez@gmail.com>
2021-09-30 01:59:43 +0000

    Mandatory solved

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """ An empty class that defines
+    a square class"""
+    def __init__(self, size):
+        self.__size = size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/u-sr/bin/python3
+
+
+class Square:
+    """ An empty class that defines
+    a square class"""
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
