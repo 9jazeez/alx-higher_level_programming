123fc6464e0f3a8d4b4fa6d63a16baa30fa0ad5e
9jazeez <9jazeez@gmail.com>
2021-10-12 03:05:35 +0000

    Error corrected

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,5 +6,5 @@ module for is_kind_of_class
 
 
 def is_kind_of_class(obj, a_class):
-        """Return if the object is an instance of a class"""
-            return isinstance(obj, a_class)
+    """Return if the object is an instance of a class"""
+    return isinstance(obj, a_class)
