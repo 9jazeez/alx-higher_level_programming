f762973965423cda5d4526d1423eafb1bc4d7ecf
9jazeez <9jazeez@gmail.com>
2021-10-13 10:28:13 +0000

    Pascal triangle added
    A function that produces pascal tringle according to the size given

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Module for pascal triangle function
+
+"""
+
+
+def pascal_triangle(n):
+    """A function that genertes a pascal triangle
+
+    Args
+    int (n): Size of the pascal triangle
+
+    Returns: A list of lists of integers
+
+    """
+
+    lists = []
+
+    if n <= 0:
+        return (lists)
+
+    lists = [[1]]
+    aux = []
+    for i in range(n - 1):
+        aux.append(1)
+        for f in range(i):
+            aux.append(lists[i][f] + lists[i][f+1])
+        aux.append(1)
+        auxc = aux[:]
+        aux = []
+        lists.append(auxc)
+
+    for i in lists:
+        print(i)
