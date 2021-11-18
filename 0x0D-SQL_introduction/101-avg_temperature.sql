182f278ab3942a2d111c62bc56b0edd52e5ab225
9jazeez <9jazeez@gmail.com>
2021-11-18 21:08:14 +0000

    Changes made

--- 0x0D-SQL_introduction/101-avg_temperature.sql
+++ 0x0D-SQL_introduction/101-avg_temperature.sql
@@ -1,3 +1,3 @@
 -- Uses the imported temperature table to display the averages
 -- by city oredered by temp (descending order)
-SELECT city AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
