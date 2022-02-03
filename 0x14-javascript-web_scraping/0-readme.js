e96b70c993b514775e4a5fa836bb61ca1ca093f9
Lawal Azeez <9jazeez@gmail.com>
2022-02-03 02:58:00 +0000

    null file checked

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,11 @@ const file = require('fs');
 
 file.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (data) {
-    console.log(data);
+    if (data === null) {
+      process.stdout.write('Empty');
+    } else {
+      process.stdout.write(data);
+    }
   } else {
     console.log(err);
   }
